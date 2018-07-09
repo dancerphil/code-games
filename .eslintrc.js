84d37bce3644b362adb14436aa15396eeb0ba5e6
Cong Zhang <dancerphil1994@gmail.com>
2018-07-09 16:52:36 +0800

    basic jest environment

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "plugins": ["jest"],
+  "env": {
+    "jest/globals": true
+  },
+  "rules": {
+    "max-len": "off", // no force newline
+    "no-bitwise": "off",
+    "no-extend-native": "off", // polyfill
+    "no-plusplus": "off",
+    "object-curly-newline": "off", // no force newline
+    "object-property-newline": "off", // no force newline
+
+    /* anti */
+    "no-param-reassign": "off"
+  }
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# Code Games
+
+basic jest environment
