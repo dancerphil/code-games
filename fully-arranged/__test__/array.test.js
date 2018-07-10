721933610268a8579c374f4f897ffb5a4eb4a254
Cong Zhang <dancerphil1994@gmail.com>
2018-07-10 16:54:38 +0800

    fully arranged

---
+++ fully-arranged/__test__/array.test.js
@@ -0,0 +1,19 @@
+import arranged, { getRest } from '../array';
+
+describe('fully arranged', () => {
+  test('value', () => {
+    expect(arranged('abc')).toEqual([
+      ['a', 'b', 'c'],
+      ['a', 'c', 'b'],
+      ['b', 'a', 'c'],
+      ['b', 'c', 'a'],
+      ['c', 'a', 'b'],
+      ['c', 'b', 'a']
+    ]);
+    expect(arranged('ab')).toEqual([['a', 'b'], ['b', 'a']]);
+  })
+  test('get rest', () => {
+    expect(getRest(['a', 'b', 'c'], 0)).toEqual({ word: ['a'], rest: ['b', 'c'] });
+    expect(getRest(['a', 'b', 'c'], 1)).toEqual({ word: ['b'], rest: ['a', 'c'] });
+  })
+});
