74e2292a667621da6a7047d1c574ab8285cfc784
Cong Zhang <dancerphil1994@gmail.com>
2018-10-06 22:28:57 +0800

    array uniq

---
+++ leetcode/polyfill/__test__/array-uniq.test.js
@@ -0,0 +1,8 @@
+import '../array-uniq';
+
+describe('polyfill array-uniq', () => {
+  test('value', () => {
+    expect([].uniq()).toEqual([]);
+    expect([1, 2, 2, 3].uniq()).toEqual([1, 2, 3]);
+  });
+});
