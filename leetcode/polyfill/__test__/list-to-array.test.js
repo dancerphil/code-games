9a217fc7ea27584908700cf72900551e0661c342
Cong Zhang <dancerphil1994@gmail.com>
2018-10-08 15:29:16 +0800

    list to array

---
+++ leetcode/polyfill/__test__/list-to-array.test.js
@@ -0,0 +1,10 @@
+import ListNode from '../../list/ListNode';
+import '../list-to-array';
+
+describe('polyfill list-to-array', () => {
+  test('value', () => {
+    const list = new ListNode(0);
+    list.next = new ListNode(1);
+    expect(list.toArray()).toEqual([0, 1]);
+  });
+});
---
+++ leetcode/polyfill/list-to-array.js
@@ -0,0 +1,16 @@
+import ListNode from '../list/ListNode';
+
+Object.defineProperty(ListNode.prototype, 'toArray', {
+  value: function toArray() {
+    const list = this;
+    let node = list;
+    const ans = [];
+    while (node !== null) {
+      const { val, next } = node;
+      ans.push(val);
+      node = next;
+    }
+    return ans;
+  },
+  writable: false,
+});
