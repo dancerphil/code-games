0703c907c183eaca9fb9d677cc80ddcc85b5bea3
Cong Zhang <dancerphil1994@gmail.com>
2020-07-25 18:03:51 +0800

    add test for number-to-array

--- leetcode/polyfill/number-to-array.js
+++ leetcode/polyfill/number-to-array.js
@@ -2,10 +2,11 @@ Object.defineProperty(Number.prototype, 'toArray', {
   value: function isSquare() {
     let number = this;
     const arr = [];
-    while (number !== 0) {
+    while (number > 0) {
       arr.push(number % 10);
       number = Math.floor(number / 10);
     }
+    arr.reverse();
     return arr;
   },
   writable: false,
