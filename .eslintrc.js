145b70482774e304184bbc3c1beca9f5544a1fec
Cong Zhang <dancerphil1994@gmail.com>
2022-06-24 19:36:36 +0800

    Update Infrastructure

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,43 @@
-module.exports = {
-  "extends": "airbnb-base",
-  "plugins": ["jest"],
-  "env": {
-    "jest/globals": true
-  },
-  "rules": {
-    "max-len": "off", // no force newline
-    "no-bitwise": "off", // using bitwise
-    "no-continue": "off", // using continue
-    "no-extend-native": "off", // polyfill
-    "no-mixed-operators": "off", // + - * / **
-    "no-nested-ternary": "off", // using nested-ternary
-    "no-plusplus": "off", // using i++
-    "object-curly-newline": "off", // no force newline
-    "object-property-newline": "off", // no force newline
+require('@reskript/config-lint/patch');
 
-    /* anti */
-    "no-param-reassign": "off"
-  }
+module.exports = {
+  extends: require.resolve('@reskript/config-lint/config/eslint'),
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    // close some rules
+    'camelcase': 'off',
+    'max-len': 'off',
+    'max-statements': 'off',
+    'no-negated-condition': 'off',
+    'prefer-promise-reject-errors': 'off',
+    '@typescript-eslint/init-declarations': 'off',
+    // open some rules
+    '@typescript-eslint/prefer-ts-expect-error': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+  },
 };
+//
+// module.exports = {
+//   "extends": "airbnb-base",
+//   "plugins": ["jest"],
+//   "env": {
+//     "jest/globals": true
+//   },
+//   "rules": {
+//     "max-len": "off", // no force newline
+//     "no-bitwise": "off", // using bitwise
+//     "no-continue": "off", // using continue
+//     "no-extend-native": "off", // polyfill
+//     "no-mixed-operators": "off", // + - * / **
+//     "no-nested-ternary": "off", // using nested-ternary
+//     "no-plusplus": "off", // using i++
+//     "object-curly-newline": "off", // no force newline
+//     "object-property-newline": "off", // no force newline
+//
+//     /* anti */
+//     "no-param-reassign": "off"
+//   }
+// };
---
+++ src/leetcode/696/__tests__/index.test.js
@@ -0,0 +1,12 @@
+import countBinarySubstrings from '../index';
+
+describe('696', () => {
+  test('countBinarySubstrings', () => {
+    expect(countBinarySubstrings('01')).toBe(1);
+    expect(countBinarySubstrings('00110')).toBe(3);
+  });
+
+  test('complex', () => {
+    expect(countBinarySubstrings('00101110')).toBe(4);
+  });
+});
